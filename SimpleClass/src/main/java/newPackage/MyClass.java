65d783391b46048b6dff187fbddf4771794e1cb6
sultana2020 <s2020sana@gmail.com>
2020-07-15 19:59:19 -0400

    my commit

---
+++ SimpleClass/src/main/java/newPackage/MyClass.java
@@ -0,0 +1,12 @@
+package newPackage;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("This is my class ");
+	}
+
+}
