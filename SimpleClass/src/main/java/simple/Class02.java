11bd6d3dc45f7e51f48468e24565e338319e7df5
sultana2020 <s2020sana@gmail.com>
2020-07-15 19:14:04 -0400

    This is commit from SimpleClass

---
+++ SimpleClass/src/main/java/simple/Class02.java
@@ -0,0 +1,12 @@
+package simple;
+
+public class Class02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("This is Class02");
+	}
+
+}
